f173987018bd8e3d9c53ef3ef18d3ea204e09696
Mbali2000 <107546127+Mbali2000@users.noreply.github.com>
2022-08-04 23:32:36 +0200

    get image file

--- ImageFilter/src/GetPixels.java
+++ ImageFilter/src/GetPixels.java
@@ -6,4 +6,19 @@ public class GetPixels{
         img = null;
         f = null;
     }
+
+    @override
+    public paint()
+
+    public static void main(String[] args) throws IOException {
+        //read in image
+        try {
+            f = new File("pic.jpg");
+            img = ImageIO.read(f);
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e);
+        }
+        
+    }
 }
--- ImageFilter/src/MeanFilterSerial.java
+++ ImageFilter/src/MeanFilterSerial.java
@@ -1,6 +1,37 @@
 public class MeanFilterSerial extends javax.swing.jpanel{
     BufferedImage img;
     File f;
+    int[][][] rgb;
 
-    
+    PUBLIC MeanFilterSerial(){
+        img = null;
+        f = null;
+    }
+
+    @override
+    public paint(Graphic g){
+        super.paint(g);
+        if(img !=null){
+            g.drawImage(img, 0, 0, null);
+        }
+    }
+
+    //get image pixels
+    public void readPixels(){
+
+    }
+
+    public static void main(String[]args) throws IOException{
+        //read in image
+        try {
+            f = new File("pic.jpg");
+            img = ImageIO.read(f);
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e);
+        }  
+
+    }
+
+
 }
