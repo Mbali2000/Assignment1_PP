ca497893b1247022c801f21fdf1bb5682846b82b
Mbali2000 <107546127+Mbali2000@users.noreply.github.com>
2022-08-05 00:18:24 +0200

    Update MeanFilterSerial.java

--- ImageFilter/src/MeanFilterSerial.java
+++ ImageFilter/src/MeanFilterSerial.java
@@ -17,7 +17,15 @@ public class MeanFilterSerial extends javax.swing.jpanel{
     }
 
     //get image pixels
-    public void readPixels(){
+    public void getPixels(){
+        for(int row = 0; row<img.getHeight(); row++){
+            for(int col =0; col<img.getHeight(); col++){
+                Color c = new Color(img.getRGB(col, row));//returns rgb value of each pixel in image
+                rgbB[0][row][col] = c.getRed();
+                rgbB[1][row][col] = c.getGreen();
+                rgbB[2][row][col] = c.getBlue();
+            }
+        }
 
     }
 
@@ -25,11 +33,21 @@ public class MeanFilterSerial extends javax.swing.jpanel{
         //read in image
         try {
             f = new File("pic.jpg");
-            img = ImageIO.read(f);
+            img = ImageIO.read(f);// assigning file to image variable
+            rgbB = newint[3][img.getHeight()][img.getWidth()];
         } catch (Exception e) {
             //TODO: handle exception
             System.out.println(e);
-        }  
+        } 
+
+        //image smoothing
+        getPixels(); //loads image pixels
+        for(int row = 1; row< img.getHeight()-1; row++){
+            for(int col = 1; col< img.getWidth() - 1; col++){
+
+                int mean =
+            }
+        }
 
     }
 
