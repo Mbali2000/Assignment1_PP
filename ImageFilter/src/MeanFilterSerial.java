ed24568fc27cdc8f7286d884ed65e01ad9fb36da
Mbali2000 <107546127+Mbali2000@users.noreply.github.com>
2022-08-06 21:05:22 +0200

    Mean Algorithm calculation
    Trying to figure out proper algorithm for calculation

--- ImageFilter/src/MeanFilterSerial.java
+++ ImageFilter/src/MeanFilterSerial.java
@@ -1,29 +1,29 @@
-public class MeanFilterSerial extends javax.swing.jpanel{
-    BufferedImage img;
-    File f;
-    int[][][] rgb;
+import java.io.*;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import javax.swing.JPanel;
+import javafx.scene.paint.Color;
 
-    PUBLIC MeanFilterSerial(){
+public class MeanFilterSerial extends JPanel{
+    static BufferedImage img;
+    static File f;
+    static int rgb[][][];
+
+    public MeanFilterSerial(){
         img = null;
         f = null;
     }
 
-    @override
-    public paint(Graphic g){
-        super.paint(g);
-        if(img !=null){
-            g.drawImage(img, 0, 0, null);
-        }
-    }
 
     //get image pixels
-    public void getPixels(){
+    public static void getPixels(){
         for(int row = 0; row<img.getHeight(); row++){
             for(int col =0; col<img.getHeight(); col++){
-                Color c = new Color(img.getRGB(col, row));//returns rgb value of each pixel in image
-                rgbB[0][row][col] = c.getRed();
-                rgbB[1][row][col] = c.getGreen();
-                rgbB[2][row][col] = c.getBlue();
+                Color c = new Color(img.getRGB(col, row), col, col, col);//returns rgb value of each pixel in image
+                rgb[0][row][col] = (int) c.getRed();   //red rgb value at row and height specific position
+                rgb[1][row][col] = (int) c.getGreen(); //green rgb value at row and height specific position
+                rgb[2][row][col] = (int) c.getBlue();  //blue rgb value at row and height specific value
             }
         }
 
@@ -32,24 +32,43 @@ public class MeanFilterSerial extends javax.swing.jpanel{
     public static void main(String[]args) throws IOException{
         //read in image
         try {
-            f = new File("pic.jpg");
+            f = new File("C:\\Users\\Thabani\\Desktop\\Assignment1_PP\\Assignment1_PP\\ImageFilter\\importimg\\Face.jpeg");
             img = ImageIO.read(f);// assigning file to image variable
-            rgbB = newint[3][img.getHeight()][img.getWidth()];
+            rgb = new int[3][img.getHeight()][img.getWidth()];
         } catch (Exception e) {
-            //TODO: handle exception
+            //handle exception
             System.out.println(e);
         } 
 
+        //get image width and height
+        int width = img.getWidth();
+        int height = img.getHeight();
+
         //image smoothing
         getPixels(); //loads image pixels
-        for(int row = 1; row< img.getHeight()-1; row++){
+        for(int row = 1; row< img.getHeight()- 1; row++){
             for(int col = 1; col< img.getWidth() - 1; col++){
+                
+                //window of 3 by 3 row and column
+                int meanRed = meanRed + rgb[0][row-1][col+1];
+                int meanGreen = meanGreen + rgb[1][row][col];
+                int meanBlue = meanBlue + rgb[2][row][col];
 
-                int mean =
+                img.setRGB(col, row, rgb);;
             }
         }
 
+        //write image
+        try {
+            f = new File("C:\\Users\\Thabani\\Desktop\\Assignment1_PP\\Assignment1_PP\\ImageFilter\\exportimg");
+            ImageIO.write(img,"jpg", f);
+        } catch (Exception e) {
+            //handle exception
+            System.out.println(e);
+        }
+
     }
+
 
 
 }
